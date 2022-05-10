00378f02002fd64d920e9bed94fe77087408f263
CGAnVil <hoangminh.tlu@gmail.com>
2022-05-10 10:02:15 +0700

    create project

---
+++ src/main/java/com/codegym/CaseNhom6M6BeApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseNhom6M6BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseNhom6M6BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/CaseNhom6M6BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseNhom6M6BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
