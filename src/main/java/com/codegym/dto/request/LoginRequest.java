0dd82278a3ce66e2019db2176e996277f4663d13
SangBui <sangnhanma14@gmail.com>
2022-05-18 09:21:15 +0700

    login-with-google

---
+++ src/main/java/com/codegym/dto/request/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.codegym.dto.request;
+
+public class LoginRequest {
+    private String email;
+    private String type;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
